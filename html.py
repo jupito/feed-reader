773f05f6626dc8ec8094e2ce32e321e15e14908c
Jussi Toivonen <jupito@iki.fi>
2015-07-04 12:24:40 +0300

    test

--- html.py
+++ html.py
@@ -10,7 +10,8 @@ def tag(name, content=None, *attributes):
     else:
         s = '<{n}{a} />'
     #return s.format(**d).encode('ascii', 'xmlcharrefreplace')
-    return s.format(**d).encode('utf-8')
+    #return s.format(**d).encode('utf-8')
+    return unicode(s.format(**d), encoding='utf-8')
 
 def href(link, content):
     return tag('a', content, ('href', link))
