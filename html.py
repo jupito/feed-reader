597e2aad023efdc06f294ecdfc6e138eade267d3
Jussi Toivonen <jupito@iki.fi>
2015-07-04 09:54:30 +0300

    rf

--- html.py
+++ html.py
@@ -40,19 +40,19 @@ def stylesheet(sheet):
             ('href', sheet))
 
 def head(title, sheet=None, redirect=None):
+    sheet=stylesheet(sheet) if sheet else ''
+    redirect=head_redirect(redirect) if redirect else ''
     s = '''<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
         "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" dir="ltr">
 <head>
 <meta http-equiv="content-type" content="text/html; charset=utf-8" />
-<title>{t}</title>
-{s}
-{r}
+<title>{title}</title>
+{sheet}
+{redirect}
 </head>
 <body>'''
-    d = dict(t=title,
-        s=stylesheet(sheet) if sheet else '',
-        r=head_redirect(redirect) if redirect else '')
+    d = dict(title=title, sheet=sheet, redirect=redirect)
     return s.format(**d)
 
 def tail():
