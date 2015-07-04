4d49b6424059f9fc690f58e40f18dfefa8140873
Jussi Toivonen <jupito@iki.fi>
2015-07-04 12:37:16 +0300

    test

--- html.py
+++ html.py
@@ -12,6 +12,7 @@ def tag(name, content=None, *attributes):
     #return s.format(**d).encode('ascii', 'xmlcharrefreplace')
     #return s.format(**d).encode('utf-8')
     #return unicode(s.format(**d), encoding='utf-8')
+    print s
     return s.format(**d).decode('utf8', errors='replace')
 
 def href(link, content):
