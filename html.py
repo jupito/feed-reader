3db4122db7151fbbe49388c1f4b7dcc3f21b17df
Jussi Toivonen <jupito@iki.fi>
2015-07-05 07:21:48 +0300

    ws

--- html.py
+++ html.py
@@ -42,8 +42,8 @@ def stylesheet(sheet):
             ('href', sheet))
 
 def head(title, sheet=None, redirect=None):
-    sheet=stylesheet(sheet) if sheet else ''
-    redirect=head_redirect(redirect) if redirect else ''
+    sheet = stylesheet(sheet) if sheet else ''
+    redirect = head_redirect(redirect) if redirect else ''
     s = '''<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
         "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" dir="ltr">
